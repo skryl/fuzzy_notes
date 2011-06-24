3b530050d2dcb19ce06b19289a8ff83760bf6dca
Alex Skryl <rut216@gmail.com>
2011-06-24 14:43:17 -0500

    added openSSL encryption via gibberish gem

--- fuzzy_notes.rb
+++ fuzzy_notes.rb
@@ -1,13 +1,14 @@
 require 'fuzzy_finder'
 require 'rubygems'
 require 'active_support/buffered_logger'
+require 'gibberish'
 
 class FuzzyNotes
 
 LOG_LEVEL = 1
 EDITOR='vim'
 NOTE_PATH=[ "#{ENV['HOME']}/Dropbox/notes" ]
-NOTE_EXTENSIONS=%w( txt )
+NOTE_EXTENSIONS=%w( txt enc )
 
 attr_reader :keywords, :notes, :all_notes
 private :keywords, :notes, :all_notes
@@ -34,6 +35,13 @@ def edit
   exec("#{EDITOR} #{bashify_paths(notes)}") if notes
 end
 
+def encrypt
+  apply_cipher
+end
+
+def decrypt
+  apply_cipher(true)
+end
 
 # view WC info for all/matching notes
 def info
@@ -41,16 +49,39 @@ def info
   puts `wc $(find #{paths} -type f)` 
 end
 
-
 private
 
-def log
-  @log || ActiveSupport::BufferedLogger.new(STDOUT, LOG_LEVEL)
+def apply_cipher(decrypt = false)
+  extension, action = decrypt ? ['.txt', 'dec'] : ['.enc', 'enc']
+  password = get_password
+  cipher = Gibberish::AES.new(password)
+  notes.each do |note|
+    puts "#{action} '#{note}'"
+    pathname = File.dirname(note)
+    filename = File.basename(note, '.*')
+    begin
+      ciphertext = cipher.send(action, File.read(note))
+      File.open("#{pathname}/#{filename}#{extension}", 'w') { |f| f << ciphertext }
+      File.delete(note)
+    rescue OpenSSL::Cipher::CipherError => e
+      puts "  ERROR: #{e}"
+    end
+  end
+end
+
+def get_password
+  printf 'Enter password (will not be shown):'
+  `stty -echo`; password = STDIN.gets.strip;`stty echo`; puts
+  password
 end
 
 # lists matching note paths in bash style, space seperated fashion
 def bashify_paths(paths)
   paths.map {|n| "\"#{n}\""}.join(' ')
+end
+
+def log
+  @log || ActiveSupport::BufferedLogger.new(STDOUT, LOG_LEVEL)
 end
 
 end
